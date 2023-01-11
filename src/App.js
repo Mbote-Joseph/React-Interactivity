8bbb6fdd95200d290eb851bd4628b1416515815c
Mbote-Joseph <mbotejoseph001@gmail.com>
2023-01-11 14:16:39 +0300

    Learning React Interactivity

--- src/App.js
+++ src/App.js
@@ -19,6 +19,16 @@ function App() {
     console.log(sculptureList[index].name);
   };
 
+  let handlePrevious = () => {
+    if (index > 1) {
+      setIndex(index - 1);
+    } else {
+      setIndex(1);
+    }
+    console.log(sculptureList[index].name);
+  };
+  // <img src={sculptureList[index].url} alt={sculptureList[index].alt} />
+
   return (
     <div className="App">
       <div className="container" id="text">
@@ -26,6 +36,7 @@ function App() {
         <p>This is a react tutorial to practice on copying HTML code</p>
       </div>
 
+      <button onClick={handlePrevious}>Previous</button>
       <button onClick={handleClick}>Next</button>
 
       <div>
@@ -35,7 +46,7 @@ function App() {
         </p>
 
         <h3>By {sculptureList[index].artist}</h3>
-        <img src={sculptureList[index].url} alt={sculptureList[index].alt} />
+
         <p>{sculptureList[index].description}</p>
       </div>
     </div>
